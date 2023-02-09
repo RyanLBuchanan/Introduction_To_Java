339fd21f51b1ecfce6bb6ae28594d0c919a804f8
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-02-09 10:21:30 -0700

    feat: Compiled else-if-statement example

--- Projects/Intro_2_Java/FirstProgram/src/program/IfStatements.java
+++ Projects/Intro_2_Java/FirstProgram/src/program/IfStatements.java
@@ -3,8 +3,27 @@ package program;
 public class IfStatements {
 	
 	public static void main(String[] args) {
-		if (true) {
-			System.out.println("Inside the if-statement");
+//		if (true) {
+//			System.out.println("Inside the if-statement");
+//		}
+		
+		String trek = "Strange New Worlds";
+		
+//		String trek = "Fury Road";
+		
+//		String pigs = "Are not flying";
+		
+		String pigs = "Flying";
+		
+		if (trek == "Strange New Worlds" && pigs == "Are not flying")
+		{
+			System.out.println("Long live Pike!!!");
+		} 
+		else if (trek == "Strange New Worlds") {
+			System.out.println("Oh Captain, my Captain!");
+		} 
+		else {
+			System.out.println("Welcome to the Thunderdome, Bitch!");
 		}
 	}
 }
