f4144de4a6bfb5ec0b4a57d60c6fd99bcdf5740d
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-02-09 09:40:43 -0700

    feat: Printed array examples

--- Projects/Intro_2_Java/FirstProgram/src/program/HelloWorld.java
+++ Projects/Intro_2_Java/FirstProgram/src/program/HelloWorld.java
@@ -24,6 +24,12 @@ public class HelloWorld {
 		// This is a reference variable
 		String myString = new String("This is a bloody String object, Mate!  It's created with a sodding constructor, ya?!");
 		
+		// These instantiate arrays
+		int[] intArray = new int[10];
+		
+		int[] intArray1 = {2, 4, 6};
+		
+		
 		System.out.println("2 + 3 = " + (myByte + myShort));
 		
 		myBoolean = myInt < 6;
@@ -39,5 +45,14 @@ public class HelloWorld {
 		System.out.println(myInt / myByte);
 		
 		System.out.println("Zere's nien decimal, ya, und nix of ze remainder!  Macht snell!");
+		
+		System.out.print("My integer arrays are below\nintArray at the second index: ");
+		System.out.println(intArray[2]);
+		System.out.print("...with a length of: ");
+		System.out.println(intArray.length);
+		System.out.print("Und\nintArray1 at the second index: ");
+		System.out.println(intArray1[2]);
+		System.out.print("...with a length of: ");
+		System.out.println(intArray1.length);
 	}
 }
