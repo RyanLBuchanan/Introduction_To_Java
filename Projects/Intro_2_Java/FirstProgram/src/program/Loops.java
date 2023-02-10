22c1972045da0e05658676d09457ceeecb0f3a7c
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-02-10 09:52:36 -0700

    feat: Completed while loop tutorial

---
+++ Projects/Intro_2_Java/FirstProgram/src/program/Loops.java
@@ -0,0 +1,12 @@
+package program;
+
+public class Loops {
+	public static void main(String[] args) {
+		int counter = 1; 
+		
+		while (counter < 10) {
+			System.out.println(counter);
+			counter++;
+		}
+	}
+}
