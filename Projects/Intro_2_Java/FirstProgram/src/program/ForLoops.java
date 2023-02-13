eabefc73efa290eccdf1e284ef7b17e1cd0adae0
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-02-13 12:16:29 -0700

    feat: Completed for loop tutorial

---
+++ Projects/Intro_2_Java/FirstProgram/src/program/ForLoops.java
@@ -0,0 +1,37 @@
+package program;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+		
+//		for (int i = 0; i <= 10; i+=2) {
+//			System.out.println(i);
+//		}
+		
+		// Fibonacci for-loop
+//		int i;
+//		int n = 1;
+//		
+//		// Initialize first and second terms
+//		int t1 = 0, t2 = 1;
+//		
+//		// Initialize the next term (third term)
+//		int nextTerm = t1 + t2;
+//		
+//		for (i = 1; i <= n; i++) {
+//			System.out.println(t1 + "\n" + t2 + "\n" + nextTerm);
+//			t1 = t2;
+//			t2 = nextTerm;
+//			nextTerm = t1 + t2;
+//			while (n <= 10) {
+//				n++;
+//			};
+//		}
+		
+		int[] array = {1, 2, 3, 4, 8, 11, 22, 45, 66};
+		
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+}
