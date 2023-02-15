1ffc274c1a85fec9f767b32d2716e347c290e070
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-02-15 08:11:23 -0700

    feat: Completed exception tutorial

---
+++ Projects/Intro_2_Java/FirstProgram/src/program/CheckedExceptions.java
@@ -0,0 +1,19 @@
+package program;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class CheckedExceptions {
+	public static void openFile() throws FileNotFoundException{
+		new FileInputStream("C:\\DoesNotExist.txt");
+	}
+	
+	public static void main(String[] args) {
+		
+		try { 
+			openFile();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
